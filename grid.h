0c994978484842f2348a8d0208ec5d46540bf076
BrandtOgden <brandt_ogden@uri.edu>
2023-03-27 18:45:09 -0400

    Added outlines for functions

--- grid.h
+++ grid.h
@@ -1,5 +1,18 @@
-class grid {
+#include <iostream>
+#include <vector>
 
+class grid {
+private:
+    // Holds the input file
+    std::vector<std::vector<int>> grid;
+    int num_rows;
+    int num_cols;
+    std::vector<std::pair<int, int>> sorted_points;
+    
+public:
+    std::pair<int , int> find_bot_left();
+    void sort_points();
+    void calculate_convex_hull();
 };
 
 
